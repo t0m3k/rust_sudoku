618dcee23b20b38cbaaf5f2a05cd32f63ce874c7
Tomasz Tracz <t@t90.dev>
2023-11-07 21:25:57 +0000

    Add egui template

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
-mod sudoku;
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result<()> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        initial_window_size: Some([400.0, 300.0].into()),
+        min_window_size: Some([300.0, 220.0].into()),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Box::new(rust_sudoku::TemplateApp::new(cc))),
+    )
+}
+
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
 fn main() {
-    let mut sudoku = sudoku::sudoku::Board::new();
-    sudoku.print();
-    println!();
-    sudoku.generate_puzzle();
-    sudoku.add_candidate(0, 0, 1);
-    sudoku.add_candidate(0, 1, 2);
-    sudoku.remove_candidate(0, 0, 1);
-    sudoku.clear_candidates(0, 1);
-    println!("{}", sudoku.is_solved());
-    sudoku.print();
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                web_options,
+                Box::new(|cc| Box::new(rust_sudoku::TemplateApp::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
 }
